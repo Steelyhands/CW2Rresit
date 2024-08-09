e1fa88358d57d6ad742364eb823246a246c2c6dc
Steelyhands <93715151+Steelyhands@users.noreply.github.com>
2024-08-09 10:29:14 +0100

    added register method to userModel.js

--- Barnardos/Models/userModel.js
+++ Barnardos/Models/userModel.js
@@ -1,3 +1,7 @@
+const Datastore = require('nedb');
+let db = new Datastore({ filename: './Barnardos/db/user.db', autoload: true });
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 class User {
     constructor(name, email, ukAddress, phoneNumber, isAdmin) {
         this.name = name;
@@ -9,12 +13,39 @@ class User {
     }
 
     // Method to register a new user
-    register() {
-        
-        
-    }
 
-   
+    register(username, name, email, phoneNumber, address, password) {
+        // Hash the password
+        bcrypt.hash(password, saltRounds).then(function(hash) {
+            // Create a new user object with the hashed password
+            let user = {
+                username: username,
+                name: name,
+                email: email,
+                phoneNumber: phoneNumber,
+                address: address,
+                password: hash, // Store the hashed password
+                role: 'user'
+            };
+    
+            // Insert the new user into the user database
+            db.insert(user, function(err) {
+                if (err) {
+                    console.log("failed to add new user:",newUser);
+                }
+                else{
+                console.log(newUser);
+                // generating unique ID
+                user.userId = newUser._id;
+                //console.log("new user ID is:",newUser._id)); work out where to add this
+                }
+            });
+            // Return user 
+            return user;
+        });
+    
+    }
+    //setting admin status for users, should only be accessable by users with isAdmin
     setAdminStatus(isAdmin) {
         if (currentUser.isAdmin) { // Assuming currentUser is the one who is currently logged in
             this.isAdmin = isAdmin;
