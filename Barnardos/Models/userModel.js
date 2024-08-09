1058ae9b639dcc066ac545232e880d44190ae00a
Steelyhands <93715151+Steelyhands@users.noreply.github.com>
2024-08-09 11:19:51 +0100

    fixed issues with userDAO

--- Barnardos/Models/userModel.js
+++ Barnardos/Models/userModel.js
@@ -1,56 +1,41 @@
-const Datastore = require('nedb');
-let db = new Datastore({ filename: './Barnardos/db/user.db', autoload: true });
+const nedb = require('gray-nedb');
+const userDB = new nedb({ filename: './db/user.db', autoload: true });
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
-class User {
-    constructor(name, email, ukAddress, phoneNumber, isAdmin) {
-        this.name = name;
+
+class UserDAO {
+    constructor(fullName, email, address, phoneNumber, isAdmin, userName) {
+        this.fullName = fullName;
         this.email = email;
-        this.ukAddress = ukAddress;
+        this.address = address;
         this.phoneNumber = phoneNumber;
-        this.userId = userId;
-        this.isAdmin = isAdmin; // This should only be set by an admin
+        this.isAdmin = isAdmin;
+        this.userName = userName;
     }
 
-    // Method to register a new user
-
-    register(username, name, email, phoneNumber, address, password) {
-        // Hash the password
-        bcrypt.hash(password, saltRounds).then(function(hash) {
-            // Create a new user object with the hashed password
-            let user = {
-                username: username,
-                name: name,
+    create(userName, fullName, email, phoneNumber, address, password) {
+        return bcrypt.hash(password, saltRounds).then(function(hash) {
+            var newUser = {
+                userName: userName,
+                fullName: fullName,
                 email: email,
                 phoneNumber: phoneNumber,
                 address: address,
-                password: hash, // Store the hashed password
+                password: hash,
                 role: 'user'
             };
     
-            // Insert the new user into the user database
-            db.insert(user, function(err) {
-                if (err) {
-                    console.log("failed to add new user:",newUser);
+            userDB.insert(newUser, function (err) 
+            {
+                if (err) // Catches any errors
+                { 
+                    console.log("Can't insert user:", userName);
                 }
-                else{
-                console.log(newUser);
-                // generating unique ID
-                user.userId = newUser._id;
-                //console.log("new user ID is:",newUser._id)); work out where to add this
-                }
+                });
             });
-            // Return user 
-            return user;
-        });
-    
     }
-    //setting admin status for users, should only be accessable by users with isAdmin
-    setAdminStatus(isAdmin) {
-        if (currentUser.isAdmin) { // Assuming currentUser is the one who is currently logged in
-            this.isAdmin = isAdmin;
-        } else {
-            throw new Error('you do not have authorrisation to perform this action.');
-        }
-    }
+
 }
+const dao = new UserDAO(userDB);
+
+module.exports = dao;
