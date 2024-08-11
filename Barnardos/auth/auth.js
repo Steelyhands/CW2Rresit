a61017909593fe1b9c04307908eaa46ae6440725
Steelyhands <93715151+Steelyhands@users.noreply.github.com>
2024-08-11 09:48:34 +0100

    added auth.js code to handle logins

--- Barnardos/auth/auth.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require("bcrypt");
-const userModel = require("../Models/userModel");
-const user = new userModel;
-const adminModel = require('../models/adminModel');
-const admin = new adminModel;
-const jwt = require("jsonwebtoken");
-const barnardosModel = require('../models/barnardosModel');
-const newLocation = new barnardosModel;
-
